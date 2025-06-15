040141e230e4534224d33f17aa8095db59dc6d03
amirmtaati <amirmtaati@gmail.com>
2025-06-15 13:47:01 +0330

    two project euler problems solved

---
+++ project-euler/project_euler_1.jl
@@ -0,0 +1,13 @@
+using Test
+
+function solve(n)
+    sum(i for i in 1:n-1 if i % 3 == 0 || i % 5 == 0)
+end
+
+solve(1000)
+
+@testset "Test" begin
+    @test solve(10) == 23
+    @test solve(1000) == 233168
+end
+
---
+++ project-euler/project_euler_2.jl
@@ -0,0 +1,21 @@
+using Test
+
+function fib(n::Integer)
+    n <= 1 && return BigInt(n)
+
+    A = [BigInt(1) BigInt(1); BigInt(1) BigInt(0)]
+    result = A^n
+
+    return result[2]
+end
+
+function solve()
+    # We need this series: 1, 2, 3, 5, 8, ...
+    # Whereas fib(n) function will produce 1, 1, 2, 3, ... therefore we need to add 1 to the index
+    return sum(f for f in (fib(n+1) for n in 2:3:100) if f <= 4_000_000)
+end
+
+@testset "Test" begin
+    @test solve() == 4613732
+end
+
